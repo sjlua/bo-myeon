ba895cde9e79eedc49784977544c523d5d11278f
Sean Lua <sjlua@Panther-14.local>
2026-01-06 21:07:44 +1100

    Added colours for use within the entire app

---
+++ constants/colours.js
@@ -0,0 +1,31 @@
+export const AppColours = {
+  dark: {
+    heroAccent: "#ff0000ff", // App hero accent color
+    heading: "#fff",
+    description: "#d3d3d3ff",
+    rating: "#eaeaeaff",
+    body: "#eaeaeaff",
+    background: "#16181bff",
+    navBarButtonFocused: "#70C0FF",
+    navBarBackground: "#1e252bff",
+    buttonText: "#eaeaeaff",
+    buttonBgColours: "#0C5085", // works with standard white text
+    iconColours: "#cececeff",
+    ratingStars: "#a2d8ffff",
+  },
+
+  light: {
+    heroAccent: "#ff0000ff", // App hero accent color
+    heading: "#201e2b",
+    description: "#686868ff",
+    rating: "#2b2b2bff",
+    body: "#2b2b2bff",
+    background: "#f0f0f0ff",
+    navBarButtonFocused: "#0C5085",
+    navBarBackground: "#fff",
+    buttonText: "#2b2b2bff",
+    buttonBgColours: "#70C0FF", // works with standard white text
+    iconColours: "#455966ff",
+    ratingStars: "#0a93d8ff",
+  },
+};
